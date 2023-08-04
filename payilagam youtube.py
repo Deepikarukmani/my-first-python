f1d73403744095f210bf669f4a1318110aa7305f
Deepikarukmani <140103694+Deepikarukmani@users.noreply.github.com>
2023-08-04 17:05:20 +0530

    Add files via upload

---
+++ payilagam youtube.py
@@ -0,0 +1,133 @@
+#print function 
+no=4.5
+print(no)
+print(type(no))
+
+#TYPE FUNCTION
+result=True
+print(result)
+print(type(result))
+
+#ID FUNCTION
+Name= "titikksha"
+print(Name)
+print(type(Name))
+print(id(Name))
+
+#LEN FUNCTION,
+Name= "titikksha"
+print(len(Name))
+
+#ROUND FUNCTION,
+number=15.0913
+print(number)
+print(round(number))
+print(round(number,2))
+print(round(number,3))
+
+
+#BINARY FUNCTION
+no=0o776
+print(no)
+
+
+no=bin(20)
+print(bin)
+
+#FLOAT Data Type
+no=1563.554
+print(float(no))
+print(type(float(no)))
+
+no1=7.7e3
+print(no1)
+
+no2=7.7E3
+print(no2)
+
+#COMPLEX DATA TYPE
+
+number= 5+10j
+print(number)
+print(type(number))
+
+no1=10+52j
+print(no1.real)
+print(no1.imag)
+
+
+#BOOLEAN DATA TYPE
+
+n1=10
+n2=20
+print(n1>n2)
+print(n1<n2)
+print(n1==n2)
+print(n1!=n2)
+
+
+#STRING DATA TYPE
+
+name= "titikksha"
+print(name)
+
+name='titikksha'
+print(name)
+
+name= "i'm deepika i am mother of titikksha and wife of gowtham"
+print(name)
+
+
+name= "deepika"
+city='vennandur'
+pincode= "637505"
+print(name,city)
+print(name+city)
+print(name+" "+city)
+
+print(name,city,pincode)
+print(name+ city+ pincode)
+
+
+"""
+if we want space in two words and add space inside the quotation
+eg: deepika+ " "+ titikksha
+"""
+
+#STRING SLICING
+
+name="monika"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+
+
+name= "deepikagowtham"
+print(len(name))
+print(name[0:9])
+print(name[-8:-1])
+print(name[0:9:2])#step operator
+print(name[0:])
+print(name[:11])
+print(name[-11:-3:1])
+
+
+
+name= "abcdefghijklmnopqrstuvwxyz"
+print(name[-1])
+
+print(name[0])
+
+print(name[7:])
+
+print(name[:10])
+
+print(name[7:19])
+print(name[0:5])
+print(name[-1:])
+print(name[5:10])
+
